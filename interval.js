ae34a186ef235ea8f46e56a163d962771188bda7
shiling <2390693765@qq.com>
2016-05-23 12:35:27 +0800

    添加动画

---
+++ interval.js
@@ -0,0 +1,23 @@
+function moveElement(elementID, final_x, final_y, interval) {
+    if (!document.getElementById) return false;
+    if (!document.getElementById(elementID)) return false;
+    var elem = document.getElementById(elementID);
+    if (elem.movement) {
+        clearTimeout(elem.movement);
+    }
+    var ypos = parseInt(elem.style.top);
+    if (xpos == final_x && ypos == final_y) {
+        return true;
+    }
+    if (xpos < final_x) {
+        xpos++;
+    }
+    if (xpos > final_x) {
+        xpos--;
+    }
+    if (ypos < final_y) {
+        ypos++;
+    }
+    if (ypos > final_y) {
+        ypos--;
+    }
